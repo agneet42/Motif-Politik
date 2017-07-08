21c47960a9372068a5879c4dfe7fafa6fdc77a99
agneet42 <agneet257@gmail.com>
2017-07-08 19:38:29 +0530

    top 500 added

--- script.py
+++ script.py
@@ -4,5 +4,5 @@ import csv
 output = csv.writer(open("output.csv","w"))    # <= 500 hits
 output1 = csv.writer(open("output1.csv","w")) # >500 hits
 count = 0
-for url in search("Royal Enfield Reviews", stop = 40):
+for url in search("Royal Enfield Reviews", stop = 500):
 	print(url)
